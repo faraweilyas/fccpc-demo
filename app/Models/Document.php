4f80dbda2a1756f54d27a267f3bbed55ad1b865f
iLyas Farawe <faraweilyas@gmail.com>
2020-04-21 23:37:44 +0100

    Adds getter to get file icon

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -19,4 +19,14 @@ class Document extends Model
             ->as('checklist_document')
             ->withTimestamps();
     }
+
+    public function getIconText()
+    {
+        $extensions     = ['pdf' => 'pdf', 'doc' => 'doc', 'docx' => 'doc', 'csv' => 'csv', 'zip' => 'zip'];
+        $path           = "/assets/backend/media/svg/";
+        $fileExtension  = pathinfo($this->file)['extension'] ?? '';
+        $extension      = $extensions[$fileExtension] ?? '';
+        $file           = (in_array($fileExtension, array_keys($extensions))) ? "files/{$extension}.svg" : 'icons/Files/File.svg';
+        return "{$path}{$file}";
+    }
 }
