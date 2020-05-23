a930f1c79a94a5fe52917957b7da773577afce5c
Kamsi Kodi <kamsikodi@gmail.com>
2020-05-23 11:29:14 +0100

    Fixed Header

--- resources/views/layouts/backend/guest/header.blade.php
+++ resources/views/layouts/backend/guest/header.blade.php
@@ -3,6 +3,7 @@
 	<title>{{ $details->title }}</title>
 	<meta name="author" content="{{ author() }}">
     <meta name="description" content="{{ $details->description }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 	<link href="{{ BE_PLUGIN.'custom/fullcalendar/fullcalendar.bundle.css' }}" rel="stylesheet" type="text/css" />
--- resources/views/layouts/backend/guest/navigation-top.blade.php
+++ resources/views/layouts/backend/guest/navigation-top.blade.php
@@ -16,11 +16,11 @@
 			<div class="topbar-item">
 				<div class="btn btn-icon btn-hover-transparent-white w-auto d-flex align-items-center btn-lg px-2">
 					<div class="d-flex flex-column text-right pr-3">
-						<span class="text-white opacity-50 font-weight-bold font-size-sm d-none d-md-inline">Applicant</span>
+						<span class="text-white opacity-50 font-weight-bold font-size-sm d-none d-md-inline">{{ \App\Models\Guest::where('tracking_id', $id)->first()->email }}</span>
 						<span class="text-white font-weight-bolder font-size-sm d-none d-md-inline">#<?= generateApplicantID(); ?></span>
 					</div>
 					<span class="symbol symbol-35">
-						<span class="symbol-label font-size-h5 font-weight-bold text-white bg-white-o-30">A</span>
+						<span class="symbol-label font-size-h5 font-weight-bold text-white bg-white-o-30">{{ ucfirst(substr(\App\Models\Guest::where('tracking_id', $id)->first()->email, 0, 1)) }}</span>
 					</span>
 				</div>
 			</div>
