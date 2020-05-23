2bf64333460584645b0d3d8c4331c89ef9c8eb50
Kamsi Kodi <kamsikodi@gmail.com>
2020-05-23 15:24:32 +0100

    Added Tracking ID to Top

---
+++ resources/views/layouts/backend/supervisor/footer-content.blade.php
@@ -0,0 +1,12 @@
+<div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
+	<!--begin::Container-->
+	<div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+		<!--begin::Copyright-->
+		<div class="text-dark order-2 order-md-1">
+			<span class="text-muted font-weight-bold mr-2"><script>document.write(new Date().getFullYear())</script> &copy;</span>
+			<a href="{{ COMPANY_LINK }}" target="_blank" class="footer-brand-logo"><img class="max-h-20px" src="{{ BE_IMAGE.'logo/techbarn-logo.png' }}" alt="techbarn logo"/></a>
+		</div>
+		<!--end::Copyright-->
+	</div>
+	<!--end::Container-->
+</div>
