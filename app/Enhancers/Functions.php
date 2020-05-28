d44f0146a67203b97aa7d735149094d2b02ed272
Kamsi Kodi <kamsikodi@gmail.com>
2020-05-28 08:45:38 +0100

    Removed Unused Code

--- app/Enhancers/Functions.php
+++ app/Enhancers/Functions.php
@@ -198,23 +198,6 @@ function datetimeToText (string $datetime, string $format="fulldate") : string
 }
 
 /**
-* Serial Number Generator
-* @param int $no
-* @return string
-*/
-function GenerateSerial($no=5, $id) {
-    $chars 	= array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-    $serial = '';
-    $max 	= count($chars)-1;
-
-    for ($i=0;$i<$no;$i++) {
-		 $serial .= (!($i % 5) && $i ? '-' : '').$chars[rand(0, $max)];
-	}
-
-    return 'GRU'.$id.'-'.$serial;
-}
-
-/**
  * Getter for application author.
  * @return string
  */
