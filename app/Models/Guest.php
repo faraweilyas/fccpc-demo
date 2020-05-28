d54f6df617b68a65b70c72fa24f2283084ef2017
iLyas Farawe <faraweilyas@gmail.com>
2020-05-28 14:43:36 +0100

    Add getters for guest

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -11,4 +11,14 @@ class Guest extends Model
     protected $fillable = [
 		'email', 'tracking_id'
 	];
+
+    public function getInitials($textStyle='strtoupper') : string
+    {
+        return textTransformer(substr($this->email, 0, 2), $textStyle);
+    }
+
+    public function getTrackingID($textStyle='strtoupper') : string
+    {
+        return textTransformer("#".$this->tracking_id, $textStyle);
+    }
 }
