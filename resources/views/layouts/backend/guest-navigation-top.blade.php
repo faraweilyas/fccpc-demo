4ce8ae5197da82f77437c8df5292ebb913a38a16
iLyas Farawe <faraweilyas@gmail.com>
2020-06-04 02:01:22 +0100

    - Adds getters - Comments getters

--- resources/views/layouts/backend/guest-navigation-top.blade.php
+++ resources/views/layouts/backend/guest-navigation-top.blade.php
@@ -1,19 +1,12 @@
 <?php $guest = App\Models\Guest::where('tracking_id', $id)->first(); ?>
 <div class="header-top">
-	<!--begin::Container-->
 	<div class="container">
-		<!--begin::Left-->
 		<div class="d-none d-lg-flex align-items-center mr-3">
-			<!--begin::Logo-->
 			<a href="{{ route('application.index', ['id' => $id]) }}" class="mr-20">
-				<h3 class="text-white text-bold font-weight-bolder text-dark">M&amp;A - FCCPC</h3>
+				<h3 class="text-white text-bold font-weight-bolder text-dark">{!! config("app.name") !!}</h3>
 			</a>
-			<!--end::Logo-->
 		</div>
-		<!--end::Left-->
-		<!--begin::Topbar-->
 		<div class="topbar">
-			<!--begin::User-->
 			<div class="topbar-item">
 				<div class="btn btn-icon btn-hover-transparent-white w-auto d-flex align-items-center btn-lg px-2">
 					<a href="#">
@@ -27,15 +20,12 @@
 					</span>
 				</div>
 			</div>
-			<!--end::User-->
-			<!--begin::User-->
 			<div class="topbar-item">
 				<div class="btn btn-icon btn-hover-transparent-white w-auto d-flex align-items-center btn-lg px-2">
 					<div class="d-flex flex-column text-right pr-3">
-						<a href="/" title="Logout" >
+						<a href="/" title="Logout">
 							<span class="text-white font-weight-bold font-size-sm d-none d-md-inline">
 								<span class="svg-icon svg-icon-xl svg-icon-white">
-									<!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo9\dist/../src/media/svg/icons\Navigation\Sign-out.svg-->
 									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 									    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 									        <rect x="0" y="0" width="24" height="24"/>
@@ -44,7 +34,6 @@
 									        <path d="M21.7928932,9.79289322 C22.1834175,9.40236893 22.8165825,9.40236893 23.2071068,9.79289322 C23.5976311,10.1834175 23.5976311,10.8165825 23.2071068,11.2071068 L20.2071068,14.2071068 C19.8165825,14.5976311 19.1834175,14.5976311 18.7928932,14.2071068 L15.7928932,11.2071068 C15.4023689,10.8165825 15.4023689,10.1834175 15.7928932,9.79289322 C16.1834175,9.40236893 16.8165825,9.40236893 17.2071068,9.79289322 L19.5,12.0857864 L21.7928932,9.79289322 Z" fill="#000000" fill-rule="nonzero" transform="translate(19.500000, 12.000000) rotate(-90.000000) translate(-19.500000, -12.000000) "/>
 									    </g>
 									</svg>
-									<!--end::Svg Icon-->
 								</span>
 								Sign Out
 							</a>
@@ -52,9 +41,6 @@
 					</div>
 				</div>
 			</div>
-			<!--end::User-->
 		</div>
-		<!--end::Topbar-->
 	</div>
-	<!--end::Container-->
 </div>
