303aaf6fb857c7b82af914d031700fe345b6624f
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-16 00:07:28 +0100

    Added Faq Model

---
+++ app/Models/Faq.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Requests extends Model
+{
+    protected $table = 'faq';
+
+    protected $fillable = [
+        'creator', 'question', 'answer', 'category'
+    ];
+}
---
+++ database/migrations/2020_06_15_230400_create_faq_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFaqTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faq', function (Blueprint $table) {
+            $table->id();
+            $table->integer('creator');
+            $table->string('question');
+            $table->mediumText('answer');
+            $table->integer('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faq');
+    }
+}
