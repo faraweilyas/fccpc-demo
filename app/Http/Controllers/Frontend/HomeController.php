797e8f4c49f3dc2c378d7610b777f795e0e943c3
iLyas Farawe <faraweilyas@gmail.com>
2020-06-20 01:56:26 +0100

    Refactors home controller

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,82 +9,76 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-	/**
-	 * Handles the home page route.
-	 * @return void
-	 */
+    /**
+     * Handles the home page.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function index()
     {
-    	$title            = APP_NAME;
-	    $description      = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
-    	$details          = details($title, $description);
-    	return view('frontend.index', compact('details'));
+        $title            = APP_NAME;
+        $description      = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
+        $details          = details($title, $description);
+        return view('frontend.index', compact('details'));
     }
 
     /**
-	 * Handles the fee calculator page route.
-	 * @return void
-	 */
+     * Handles the fee calculator page.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function feeCalcutor()
     {
-    	$title          = "Fee Calculator - ".APP_NAME;
-	    $description    = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
-    	$details          = details($title, $description);
-    	return view('frontend.fee-calculator', compact('details'));
+        $title          = "Fee Calculator - ".APP_NAME;
+        $description    = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
+        $details        = details($title, $description);
+        return view('frontend.fee-calculator', compact('details'));
     }
 
     /**
-     * Handles the faq page route.
-     * @return void
+     * Handles the faqs page.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-    public function faq($type = null)
+    public function faqs()
     {
-        if ($type != null):
-            $faq          = Faq::where('category', $type)->get();
-        else :
-            $faq          = Faq::all();
-        endif;
-
-        $title            = "Frequently Asked Questions (FAQs) - Federal Competition and Consumer Protection Commission - ".APP_NAME;
-        $description      = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
-        $details          = details($title, $description);
-        return view('frontend.faq', compact('details', 'faq', 'type'));
+        $category       = request('category');
+        $faqs           = (is_null($category))
+                        ? Faq::all()
+                        : Faq::where('category', $category)->get();
+        $title          = "Frequently Asked Questions (FAQs) - ".APP_NAME;
+        $description    = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
+        $details        = details($title, $description);
+        return view('frontend.faqs', compact('details', 'faqs', 'category'));
     }
 
     /**
-     * Handles the faq page route.
-     * @return void
+     * Handles the faq page.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-    public function faqDetails($id)
+    public function faq(Faq $faq)
     {
-        $faq              = Faq::find($id);
-        $title            = "Frequently Asked Questions (FAQs) - Federal Competition and Consumer Protection Commission - ".APP_NAME;
-        $description      = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
-        $details          = details($title, $description);
-        return view('frontend.faq-single', compact('details', 'id', 'faq'));
+        $title          = "Frequently Asked Questions (FAQs) - ".APP_NAME;
+        $description    = "FCCPC is the apex consumer protection agency in Nigeria established to improve the well-being of the people.";
+        $details        = details($title, $description);
+        return view('frontend.faq-single', compact('details', 'faq'));
     }
 
     /**
-     * Handles the update faq feedback route.
-     * @return void
+     * Handles the store feedback route.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
-    public function updateFaqFeedback($id, $question)
+    public function storeFeedback(Faq $faq)
     {
-        $ip = request()->ip();
-        if(Feedback::where('question_id', $question)->where('ip_address', $ip)->first()):
-            Feedback::where('question_id', $question)->where('ip_address', $ip)->update([
-                'feedback' => $id
-            ]);
-            return redirect()->back()->with('success', 'Feedback saved successfully');
-        else:
-            Feedback::create([
-                'question_id' => $question,
-                'ip_address' => $ip,
-                'feedback' => $id
-            ]);
-            return redirect()->back()->with('success', 'Feedback saved successfully');
-        endif;
+        Feedback::updateOrcreate([
+            'ip_address'    => request()->ip(),
+            'question_id'   => $faq->id,
+        ], [
+            'feedback'      => request('feedback')
+        ]);
 
-        return redirect()->back();
+        return back()->with('success', 'Thanks for your feedback');
     }
 }
