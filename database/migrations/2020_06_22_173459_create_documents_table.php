899a52cfbaf72b763ed0e19b7a5d49da080214d4
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:27:19 +0100

    Adds document migration

---
+++ database/migrations/2020_06_22_173459_create_documents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('case_id');
+            $table->sting('group', 50)->nullable();
+            $table->string('file');
+            $table->text('additional_info')->nullable();
+            $table->timestamps();
+
+            $table->foreign('case_id')
+                ->references('id')
+                ->on('cases')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
