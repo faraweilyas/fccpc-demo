d3387e6cebeca04f27b270a6372ac7b913a40f6a
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:24:54 +0100

    Registers facades

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,31 +3,13 @@
 namespace App\Providers;
 
 use App\Enhancers\AppHelper;
+use App\Enhancers\SerialNumber;
 use App\Enhancers\AppRepository;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        app()->singleton(AppHelper::class, function($app)
-        {
-            return new AppHelper($app);
-        });
-
-        app()->singleton(AppRepository::class, function($app)
-        {
-            return new AppRepository($app);
-        });
-    }
-
     /**
      * Bootstrap any application services.
      *
@@ -38,5 +20,43 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         forceHTTPSScheme();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerSerialNumber();
+
+        $this->registerAppHelper();
+
+        $this->registerAppRepository();
+    }
+
+    public function registerSerialNumber()
+    {
+        app()->singleton('serialnumber', function($app)
+        {
+            return new SerialNumber($app);
+        });
+    }
+
+    public function registerAppHelper()
+    {
+        app()->singleton('apphelper', function($app)
+        {
+            return new AppHelper($app);
+        });
+    }
+
+    public function registerAppRepository()
+    {
+        app()->singleton('apprepository', function($app)
+        {
+            return new AppRepository($app);
+        });
     }
 }
