9aa774356773ee4d0f7fd199a312da0150587adf
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:24:02 +0100

    - Rearranges columns - Modifies getters

--- app/Models/Cases.php
+++ app/Models/Cases.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-use App\User;
-use App\Enhancers\AppHelper;
+use App\Models\User;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,14 +11,13 @@ class Cases extends Model
 	protected $table = 'cases';
 
     protected $fillable = [
-		'ref_no', 'tracking_id', 'subject', 'parties', 'transaction_type', 'transaction_category', 'application_firm', 'applicant_first_name',
-        'applicant_last_name', 'applicant_email', 'applicant_phone_no', 'applicant_address', 'applicant_company_documents',
-        'applicant_account_documents', 'applicant_payment_documents', 'status', 'case_handler_id', 'recommendation', 'comments', 'request_id'
+		'user_id', 'guest_id', 'reference_number', 'subject', 'parties', 'case_category', 'case_type',
+        'applicant_firm', 'applicant_first_name', 'applicant_last_name', 'applicant_email', 'applicant_phone_no', 'applicant_address'
 	];
 
     public function getRefNO($textStyle='strtoupper') : string
     {
-        $refrenceNo = !empty($this->ref_no) ? "#{$this->ref_no}" : '';
+        $refrenceNo = !empty($this->reference_number) ? "#{$this->reference_number}" : '';
         return textTransformer($refrenceNo, $textStyle);
     }
 
@@ -41,12 +39,12 @@ class Cases extends Model
 
     public function getCaseStatus($textStyle='strtolower') : string
     {
-        return textTransformer(AppHelper::$case_status[$this->status] ?? "", $textStyle);
+        return \AppHelper::value('case_status', $this->status, $textStyle);
     }
 
     public function getCaseStatusHTML($textStyle='strtolower') : string
     {
-        return textTransformer(AppHelper::$case_statusHTML[$this->status] ?? "", $textStyle);
+        return \AppHelper::value('case_status_html', $this->status, $textStyle);
     }
 
     public function getCaseHandlerName() : string
@@ -56,7 +54,7 @@ class Cases extends Model
 
     public function getCaseCategory($textStyle='strtolower') : string
     {
-        return textTransformer(AppHelper::$case_categories[$this->transaction_category] ?? "", $textStyle);
+        return \AppHelper::value('case_categories', $this->case_category, $textStyle);
     }
 
     public function getTransactionType($textStyle='ucfirst') : string
