fe7dd706a3fd0e50a09a6558d28f6d3a3c67abc3
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:29:20 +0100

    Configures user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,19 +6,15 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
-$factory->define(User::class, function (Faker $faker)
+$factory->define(User::class, function(Faker $faker)
 {
     return [
-        'account_type'      => $faker->randomElement([
-            'root', "admin", "it personnel", 'director general', "supervisor", "case handler"
-        ]),
+        'account_type'      => $faker->randomElement(AppHelper::keys('account_types')),
         'first_name'        => $faker->firstName,
         'last_name'         => $faker->lastName,
         'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'status'            => $faker->randomElement([
-            'active', 'inactive'
-        ]),
+        'status'            => $faker->randomElement(\AppHelper::keys('status')),
         'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token'    => Str::random(10),
     ];
