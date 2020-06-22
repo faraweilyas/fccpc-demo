790b3bdb9484114f1a5c92efabd83c4d600d156b
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:19:41 +0100

    Adds comments

--- app/Enhancers/AppRepository.php
+++ app/Enhancers/AppRepository.php
@@ -7,8 +7,18 @@ namespace App\Enhancers;
  */
 class AppRepository
 {
+    /**
+     * $app App instance
+     * @var null|Illuminate\Foundation\Application
+     */
     protected $app;
 
+    /**
+     * Loads the app instance into scope
+     *
+     * @param null|Illuminate\Foundation\Application $app
+     * @return void
+     */
     public function __construct($app=null)
     {
         $this->app = is_null($app) ? app() : $app;
