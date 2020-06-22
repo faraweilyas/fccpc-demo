8d9046b41969d80bd7dd4fa67ad6fe0a32a2fa22
iLyas Farawe <faraweilyas@gmail.com>
2020-06-22 23:22:28 +0100

    Restructures model

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guest extends Model
 {
-	protected $table = 'guest';
-
     protected $fillable = [
-		'email', 'tracking_id'
+		'tracking_id', 'email'
 	];
 
     /**
