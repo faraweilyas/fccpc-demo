5bdf246011850344094d69721c80131102f88f5e
iLyas Farawe <faraweilyas@gmail.com>
2020-06-23 14:37:14 +0100

    Made some text changes

--- resources/views/backend/faq/logs.blade.php
+++ resources/views/backend/faq/logs.blade.php
@@ -5,13 +5,13 @@
         <div class="container d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
             <div class="d-flex align-items-center flex-wrap mr-1">
                 <div class="d-flex align-items-baseline mr-5">
-                    <h5 class="text-dark font-weight-bold my-2 mr-5">Faq Log</h5>
+                    <h5 class="text-dark font-weight-bold my-2 mr-5">All FAQs</h5>
                     <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
                         <li class="breadcrumb-item">
                             <a href="{{ route('dashboard.index') }}" class="text-muted">Home</a>
                         </li>
                         <li class="breadcrumb-item">
-                            <a href="" class="text-muted">Faq Log</a>
+                            <a href="" class="text-muted">All FAQs</a>
                         </li>
                     </ul>
                 </div>
@@ -24,7 +24,7 @@
                 <div class="card card-custom">
                     <div class="card-header flex-wrap py-5">
                         <div class="card-title">
-                            <h3 class="card-label">Enquiries Log</h3>
+                            <h3 class="card-label">All FAQs</h3>
                         </div>
                     </div>
                     <div class="card-body">
