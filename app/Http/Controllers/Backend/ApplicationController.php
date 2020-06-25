0be49506872f5af9cf4bdaaa73829544fe91737e
iLyas Farawe <faraweilyas@gmail.com>
2020-06-25 04:54:38 +0100

    - Refactor view and action methods

--- app/Http/Controllers/Backend/ApplicationController.php
+++ app/Http/Controllers/Backend/ApplicationController.php
@@ -2,92 +2,193 @@
 
 namespace App\Http\Controllers\Backend;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
-use App\Http\Controllers\Controller;
 use App\Models\Cases;
 use App\Models\Guest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
 
 class ApplicationController extends Controller
 {
+    protected $methods          = [
+        'saveCaseInfo'          => 'saveCaseInfo',
+        'saveContactInfo'       => 'saveContactInfo',
+        'saveCompanyDocuments'  => 'saveDocument',
+        'saveAccountDocuments'  => 'saveDocument',
+        'savePaymentDocuments'  => 'saveDocument',
+    ];
+
 	/**
-     * Handles the select application page route.
-     * @param int $id
-     * @return void
+     * Handles select application page.
+     *
+     * @param Guest $guest
+     * @return \Illuminate\Contracts\View\Factory
      */
-    public function index($id)
+    public function index(Guest $guest)
     {
-        if (!Guest::where('tracking_id', '=', $id)->first()) {
-            return redirect()->route('applicant.submit')->with("error", "Invalid Credentials!");
-        }
+        if ($guest->case->isSubmitted())
+            return redirect($guest->submittedApplicationPath());
 
-        if ($case = Cases::where('tracking_id', '=', $id)->first()):
-            if ($case->status > 0):
-                return redirect()->route('application.success', ['id' => $id]);
-            endif;
-        endif;
-
-        $title            = APP_NAME;
-        $description      = "FCCPC Select Application Dashboard";
+        $title            = 'Select Application | '.APP_NAME;
+        $description      = 'Select Application | '.APP_NAME;
         $details          = details($title, $description);
-        return view('backend.applicant.select-application', compact('details', 'id'));
+        return view('backend.applicant.select-application', compact('details', 'guest'));
     }
 
     /**
-     * Handles the application success page route.
-     * @param int $id
-     * @return void
+     * Handles create application page.
+     *
+     * @param Guest $guest
+     * @param string $case_category_key
+     * @return \Illuminate\Contracts\View\Factory
      */
-    public function applicationSuccess($id)
+    public function show(Guest $guest, string $case_category_key)
     {
-        $title            = APP_NAME;
-        $description      = "FCCPC Select Application Dashboard";
-        $details          = details($title, $description);
-        return view('backend.applicant.application-success', compact('details', 'id'));
+        // Validate case category
+        $result = !\AppHelper::validateKey('case_categories', strtoupper($case_category_key));
+        abort_if($result, 404);
+
+        $case = $guest->case;
+        // Check if case has been submitted
+        if ($case->isSubmitted())
+            return redirect($guest->submittedApplicationPath());
+
+        // Save selected case category
+        $case->saveCategory($case_category_key);
+
+        $case_category  = $case->getCaseCategory();
+        $case_parties   = $case->getCaseParties(false);
+        $title          = "{$case_category} Application | ".APP_NAME;
+        $description    = "{$case_category} Application | ".APP_NAME;
+        $details        = details($title, $description);
+        return view('backend.applicant.create-application', compact('details', 'guest', 'case', 'case_category', 'case_parties'));
     }
 
     /**
-     * Handles the create application page route.
-     * @param string $type
-     * @param int $id
-     * @return void
+     * Handles application submitted page route.
+     *
+     * @param Guest $guest
+     * @return \Illuminate\Contracts\View\Factory
      */
-    public function create($type, $id)
+    public function applicationSubmitted(Guest $guest)
     {
-        $case             = formatApplicationType($type);
-        $case_info        = Cases::where('tracking_id', '=', $id)->first();
-
-        if ($case_info):
-            if ($case_info->status > 0):
-                return redirect()->route('application.success', ['id' => $id]);
-            endif;
-
-            $case_party_arr = explode(',', $case_info->parties);
-        else:
-            $case_party_arr = '';
-        endif;
-
-        $title            = APP_NAME;
-        $description      = "FCCPC ".$case." Application Dashboard";
-        $details          = details($title, $description);
-        return view('backend.applicant.create-application', compact('details', 'id', 'type', 'case', 'case_info', 'case_party_arr'));
+        $title          = 'Application Submitted | '.APP_NAME;
+        $description    = 'Application Submitted | '.APP_NAME;
+        $details        = details($title, $description);
+        return view('backend.applicant.submitted', compact('details', 'guest'));
     }
 
     /**
-     * Handles the upload supporting documents application page route.
-     * @param int $id
+     * Send response.
+     *
+     * @param string $message
+     * @param string $responseType
+     * @param mixed $response
      * @return void
      */
-    public function supportingDocuments($id)
+    public function sendResponse(string $message, string $responseType, $response=null)
     {
+        echo json_encode([
+            'message'       => $message,
+            'responseType'  => $responseType,
+            'response'      => $response,
+        ]);
+        exit;
+    }
+
+    /**
+     * Proxy to create new case.
+     *
+     * @param Guest $guest
+     * @param string $action
+     * @return void
+     */
+    public function create(Guest $guest, string $action)
+    {
+        if (!in_array($action, array_keys($this->methods)))
+            $this->sendResponse("Something went wrong, please try again", "error");
+
+        $method = $this->methods[$action];
+        call_user_func([$this, $method], $guest);
+
+        return;
+    }
+
+    public function saveCaseInfo(Guest $guest)
+    {
+        $parties = is_array(request('parties')) ? request('parties') : [];
+
+        $guest->case->saveCaseInfo(
+            request('subject'),
+            implode(':', $parties),
+            request('case_type'),
+        );
+
+        $this->sendResponse("Case info saved.", "success", $guest->case);
+    }
+
+    public function saveContactInfo(Guest $guest)
+    {
+        $guest->case->saveContactInfo((object) [
+            'applicant_firm'            => request('applicant_firm'),
+            'applicant_first_name'      => request('applicant_first_name'),
+            'applicant_last_name'       => request('applicant_last_name'),
+            'applicant_email'           => request('applicant_email'),
+            'applicant_phone_number'    => request('applicant_phone_number'),
+            'applicant_address'         => request('applicant_address'),
+        ]);
+
+        $this->sendResponse("Contact info saved.", "success", $guest->case);
+    }
+
+    public function saveDocument(Guest $guest)
+    {
+        // $company_doc    = !empty($_FILES['company_doc']['name']) ? $_FILES['company_doc'] : [];
+        // $newFileName    = substr(uniqid(), 5, 13).".$id";
+    }
+
+    /**
+     * Upload New Case.
+     *
+     * @param string $id
+     * @return void
+     */
+    public function uploadNewCase(Request $request, $id)
+    {
+        Cases::where('tracking_id', '=', $id)->update([
+            'ref_no' => generateRefNo($id),
+            'status' => 1
+        ]);
+
+        $case = Cases::where('tracking_id', '=', $id)->first();
+        Mail::to($case->applicant_email)->send(new ApplicationRequest([
+            'firstName'       => $case->applicant_first_name,
+            'lastName'        => $case->applicant_last_name,
+            'ref_no'          => $case->ref_no
+        ]));
+        $this->sendResponse(200, "OK!", "success", $case);
+    }
+
+    /**
+     * Handles upload documents page
+     *
+     * @param Guest $guest
+     * @return \Illuminate\Contracts\View\Factory
+     */
+    public function uploadDocuments(Guest $guest)
+    {
+        if (!$guest->case->isSubmitted())
+            return redirect($guest->submittedApplicationPath());
+
         $case = Cases::where('tracking_id', '=', $id)->first();
         if ($case->status <= 0 && !is_null($case->transaction_category)):
             return redirect()->route('application.create', ['type' => $case->getCaseCategory(), 'id' => $id])->with("error", "Please complete your application!");
         endif;
 
-        $title            = APP_NAME;
-        $description      = "FCCPC Upload Support Documents";
-        $details          = details($title, $description);
-        return view('backend.applicant.uploading-documents', compact('details', 'id'));
+        $title          = 'Upload Documents | '.APP_NAME;
+        $description    = 'Upload Documents | '.APP_NAME;
+        $details        = details($title, $description);
+        return view('backend.applicant.upload-documents', compact('details', 'id'));
     }
 }
