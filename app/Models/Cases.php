e711d4c8b84a7d6ee087fe52a5133299ad113006
iLyas Farawe <faraweilyas@gmail.com>
2020-06-25 04:26:58 +0100

    Add methods to save case

--- app/Models/Cases.php
+++ app/Models/Cases.php
@@ -17,6 +17,25 @@ class Cases extends Model
         return $this->save();
     }
 
+    public function saveCaseInfo($subject, $parties, $case_type) : bool
+    {
+        $this->subject      = $subject;
+        $this->parties      = $parties;
+        $this->case_type    = $case_type;
+        return $this->save();
+    }
+
+    public function saveContactInfo(\stdClass $contactInfo) : bool
+    {
+        $this->applicant_firm           = $contactInfo->applicant_firm;
+        $this->applicant_first_name     = $contactInfo->applicant_first_name;
+        $this->applicant_last_name      = $contactInfo->applicant_last_name;
+        $this->applicant_email          = $contactInfo->applicant_email;
+        $this->applicant_phone_number   = $contactInfo->applicant_phone_number;
+        $this->applicant_address        = $contactInfo->applicant_address;
+        return $this->save();
+    }
+
     public function creator()
     {
         if (!empty($this->user_id))
