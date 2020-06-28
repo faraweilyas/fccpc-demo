6529a07427c4fdb85fd99f6cf49676ab5eda585e
iLyas Farawe <faraweilyas@gmail.com>
2020-06-28 22:47:54 +0100

    Uses saveable and assignable traits

--- app/Models/Cases.php
+++ app/Models/Cases.php
@@ -7,41 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cases extends Model
 {
+    use CaseSaveable, CaseAssignable;
+
 	protected $table = 'cases';
 
     protected $guarded = [];
-
-    public function saveCategory(string $case_category) : bool
-    {
-        $this->case_category = strtoupper($case_category);
-        return $this->save();
-    }
-
-    public function saveCaseInfo($subject, $parties, $case_type) : bool
-    {
-        $this->subject      = $subject;
-        $this->parties      = $parties;
-        $this->case_type    = $case_type;
-        return $this->save();
-    }
-
-    public function saveContactInfo(\stdClass $contactInfo) : bool
-    {
-        $this->applicant_firm           = $contactInfo->applicant_firm;
-        $this->applicant_first_name     = $contactInfo->applicant_first_name;
-        $this->applicant_last_name      = $contactInfo->applicant_last_name;
-        $this->applicant_email          = $contactInfo->applicant_email;
-        $this->applicant_phone_number   = $contactInfo->applicant_phone_number;
-        $this->applicant_address        = $contactInfo->applicant_address;
-        return $this->save();
-    }
-
-    public function submit() : bool
-    {
-        $this->reference_number = \SerialNumber::referenceNumber();
-        $this->submitted_at     = now();
-        return $this->save();
-    }
 
     public function creator()
     {
@@ -134,6 +104,6 @@ class Cases extends Model
 
     public function getCaseHandlerName() : string
     {
-        return ($caseHandler = User::find($this->case_handler_id)) ? $caseHandler->getFullName() : "";
+        return ($caseHandler = $this->handler->first()) ? $caseHandler->getFullName() : "";
     }
 }
