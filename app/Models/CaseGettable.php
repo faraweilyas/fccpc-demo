1259dd23039826e6f63ba1e65a5bd2e6ae1f575e
iLyas Farawe <faraweilyas@gmail.com>
2020-07-01 02:59:42 +0100

    - Restructures the query builder - Add comments to describe functionalities - Renames relationship methods

--- app/Models/CaseGettable.php
+++ app/Models/CaseGettable.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 trait CaseGettable
 {
+    /**
+     * Gets submitted cases
+     *
+     * @return Collection
+     */
     public function submittedCases()
     {
         return static::where('submitted_at', '!=', null)->get();
     }
 
-    public function unasignedCases()
+    /**
+     * Gets unassigned cases with handlers
+     *
+     * @return Collection
+     */
+    public function unassignedCases()
     {
         return static::where('submitted_at', '!=', null)
             ->with('handlers')
@@ -21,27 +31,51 @@ trait CaseGettable
             });
     }
 
-    public function asignedCases()
+    /**
+     * Gets assigned cases with active handlers
+     *
+     * @return Collection
+     */
+    public function assignedCases()
     {
         return static::where('submitted_at', '!=', null)
-            ->with(['handlers' => function($query)
-            {
-                $query->where('dropped_at', '=', null);
-            }])
+            ->with('active_handlers')
             ->latest()
             ->get()
             ->reject(function($case)
             {
-                return $case->handlers->isEmpty() ? true : false;
+                return $case->active_handlers->isEmpty() ? true : false;
             });
     }
 
+    /**
+     * Gets assigned cases with dropped handlers
+     *
+     * @return Collection
+     */
+    public function assignedCasesWithDroppedHandlers()
+    {
+        return static::where('submitted_at', '!=', null)
+            ->with('dropped_handlers')
+            ->latest()
+            ->get()
+            ->reject(function($case)
+            {
+                return $case->dropped_handlers->isEmpty() ? true : false;
+            });
+    }
+
+    /**
+     * Gets archived cases with handlers
+     *
+     * @return Collection
+     */
     public function archivedCases()
     {
         return static::where('submitted_at', '!=', null)
             ->with(['handlers' => function($query)
             {
-                $query->where('dropped_at', '!=', null);
+                $query->where('archived_at', '!=', null);
             }])
             ->latest()
             ->get()
