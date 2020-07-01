4cc46b4ab98a5dc7e7218fbfbdad60550dc98e5f
iLyas Farawe <faraweilyas@gmail.com>
2020-07-01 02:55:03 +0100

    - Add comments to describe functionalities - Renames relationship methods

--- app/Models/CaseAssignable.php
+++ app/Models/CaseAssignable.php
@@ -4,15 +4,34 @@ namespace App\Models;
 
 trait CaseAssignable
 {
+    /**
+     * A supervisor assigns a new case handler to a case
+     *
+     * @param  User $caseHandler
+     * @param  User|null $supervisor
+     * @return array
+     */
     public function assign(User $caseHandler, User $supervisor = null)
     {
         $supervisor_id = !is_null($supervisor) ? $supervisor->id : auth()->id();
 
-        return $this->handlers()->syncWithoutDetaching([$caseHandler->id => [
-            'supervisor_id' => $supervisor_id
-        ]]);
+        return $this->handlers()->syncWithoutDetaching([
+            $caseHandler->id    => [
+                'supervisor_id' => $supervisor_id
+            ]
+        ]);
     }
 
+    /**
+     * Sets the dropped at timestamp to indicate that a case handler has been dropped
+     * from the case but the history of the case with the case handler can be viewed
+     * after that it assigns a new case handler to case to continue where it was left off
+     *
+     * @param  User $currentCaseHandler
+     * @param  User $newCaseHandler
+     * @param  User|null $supervisor
+     * @return array
+     */
     public function reAssign(User $currentCaseHandler, User $newCaseHandler, User $supervisor = null)
     {
         $supervisor_id = !is_null($supervisor) ? $supervisor->id : auth()->id();
@@ -28,16 +47,33 @@ trait CaseAssignable
         ]);
     }
 
+    /**
+     * Sets the dropped at timestamp to indicate that a case handler has been dropped
+     * from the case but the history of the case with the case handler can be viewed
+     *
+     * @param  User $caseHandler
+     * @param  User|null $supervisor
+     * @return array
+     */
     public function retract(User $caseHandler, User $supervisor = null)
     {
         $supervisor_id = !is_null($supervisor) ? $supervisor->id : auth()->id();
 
-        return $this->handlers()->syncWithoutDetaching([$caseHandler->id => [
-            'supervisor_id' => $supervisor_id,
-            'dropped_at'    => now()
-        ]]);
+        return $this->handlers()->syncWithoutDetaching([
+            $caseHandler->id    => [
+                'supervisor_id' => $supervisor_id,
+                'dropped_at'    => now()
+            ]
+        ]);
     }
 
+    /**
+     * Removes case handler and supervisors relationship from case
+     * Which deletes the entire relationship from the database
+     *
+     * @param  User $caseHandler
+     * @return int
+     */
     public function disolve(User $caseHandler)
     {
         return $this->handlers()->detach($caseHandler);
@@ -46,17 +82,17 @@ trait CaseAssignable
     public function handlers()
     {
         return $this->belongsToMany(User::class, 'case_handler', 'case_id', 'handler_id')
-            ->withPivot('supervisor_id', 'dropped_at')
             ->as('case_handler')
+            ->withPivot('supervisor_id', 'dropped_at', 'archived_at')
             ->withTimestamps();
     }
 
-    public function handler()
+    public function active_handlers()
     {
         return $this->handlers()->where('dropped_at', null);
     }
 
-    public function droppedHandlers()
+    public function dropped_handlers()
     {
         return $this->handlers()->where('dropped_at', '!=', null);
     }
@@ -65,12 +101,7 @@ trait CaseAssignable
     {
         return $this->belongsToMany(User::class, 'case_handler', 'case_id', 'supervisor_id')
             ->as('case_supervisor')
-            ->withPivot('handler_id', 'dropped_at')
+            ->withPivot('handler_id', 'dropped_at', 'archived_at')
             ->withTimestamps();
-    }
-
-    public function supervisor()
-    {
-        return $this->supervisors()->where('dropped_at', null);
     }
 }
