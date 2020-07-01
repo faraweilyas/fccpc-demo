33bbb300f29c32a7643317f511b448d6e4e84205
iLyas Farawe <faraweilyas@gmail.com>
2020-07-01 08:06:01 +0100

    Adds user gettable trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Auth;
+use App\Models\UserTraits\UserGettable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, UserGettable;
 
     /**
      * The attributes that are mass assignable.
@@ -37,11 +38,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function cases()
-    {
-        return $this->hasMany(Cases::class);
-    }
 
     /**
      * Get users first name
@@ -75,6 +71,7 @@ class User extends Authenticatable
 
     /**
      * Get method for initials.
+     *
      * @return string
      */
     public function getInitials() : string
---
+++ app/Models/UserTraits/UserGettable.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Models\UserTraits;
+
+use App\Models\User;
+use App\Models\Cases;
+
+trait UserGettable
+{
+    /**
+     * Defines a one to many relationship for user that has created one or more cases
+     *
+     * @return HasRelationships
+     */
+    public function cases()
+    {
+        return $this->hasMany(Cases::class);
+    }
+
+    /**
+     * Defines a many to many relationship for user that has been assigned to one or more cases
+     *
+     * @return HasRelationships
+     */
+    public function cases_assigned_to()
+    {
+        return $this->belongsToMany(Cases::class, 'case_handler', 'handler_id', 'case_id')
+            ->as('case_handler')
+            ->withPivot('supervisor_id', 'dropped_at', 'archived_at')
+            ->withTimestamps();
+    }
+
+    /**
+     * Defines a many to many relationship for case and active case handlers
+     *
+     * @return HasRelationships
+     */
+    public function active_cases_assigned_to()
+    {
+        return $this->cases_assigned_to()->where('dropped_at', null);
+    }
+
+    /**
+     * Defines a many to many relationship for case and dropped case handlers
+     *
+     * @return HasRelationships
+     */
+    public function dropped_cases_assigned_to()
+    {
+        return $this->cases_assigned_to()->where('dropped_at', '!=', null);
+    }
+
+    /**
+     * Defines a many to many relationship for user that has assigned
+     * another user to one or more cases
+     *
+     * @return HasRelationships
+     */
+    public function cases_assigned_by()
+    {
+        return $this->belongsToMany(Cases::class, 'case_handler', 'supervisor_id', 'case_id')
+            ->as('case_handler')
+            ->withPivot('handler_id', 'dropped_at', 'archived_at')
+            ->withTimestamps();
+    }
+
+    /**
+     * Defines a many to many relationship for case and active case handlers
+     *
+     * @return HasRelationships
+     */
+    public function active_cases_assigned_by()
+    {
+        return $this->cases_assigned_by()->where('dropped_at', null);
+    }
+
+    /**
+     * Defines a many to many relationship for case and dropped case handlers
+     *
+     * @return HasRelationships
+     */
+    public function dropped_cases_assigned_by()
+    {
+        return $this->cases_assigned_by()->where('dropped_at', '!=', null);
+    }
+}
