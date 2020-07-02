e776c35060ae84f2f18fe38b8fe67c5712aad6be
iLyas Farawe <faraweilyas@gmail.com>
2020-07-02 13:52:36 +0100

    Restructures case handler migration

--- database/migrations/2020_06_28_045454_create_case_handler_table.php
+++ database/migrations/2020_06_28_045454_create_case_handler_table.php
@@ -13,28 +13,37 @@ class CreateCaseHandlerTable extends Migration
      */
     public function up()
     {
-        Schema::create('case_handler', function (Blueprint $table)
+        Schema::create('case_handler', function(Blueprint $table)
         {
             $table->id();
             $table->unsignedBigInteger('supervisor_id');
-            $table->unsignedBigInteger('handler_id');
             $table->unsignedBigInteger('case_id');
+            $table->unsignedBigInteger('handler_id');
+            $table->timestamp('defficiency_issued_at')->nullable();
+            $table->text('defficiency')->nullable();
+            $table->timestamp('recommendation_issued_at')->nullable();
+            $table->text('recommendation')->nullable();
+            $table->timestamp('approval_requested_at')->nullable();
+            $table->string('approval_status', 20)->nullable();
+            $table->timestamp('extension_requested_at')->nullable();
+            $table->string('extension_reason')->nullable();
             $table->timestamp('dropped_at')->nullable();
+            $table->timestamp('archived_at')->nullable();
             $table->timestamps();
 
-            $table->unique(['supervisor_id', 'handler_id', 'case_id']);
+            $table->unique(['supervisor_id', 'case_id', 'handler_id']);
 
             $table->foreign('supervisor_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
-            $table->foreign('handler_id')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade');
             $table->foreign('case_id')
                 ->references('id')
                 ->on('cases')
+                ->onDelete('cascade');
+            $table->foreign('handler_id')
+                ->references('id')
+                ->on('users')
                 ->onDelete('cascade');
         });
     }
