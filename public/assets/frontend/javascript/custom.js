b2800beebc16a46ff92854c0b147afc3cb249e40
iLyas Farawe <faraweilyas@gmail.com>
2020-07-06 12:29:33 +0100

    Deletes unused assets

--- public/assets/frontend/javascript/custom.js
+++
@@ -1,8 +0,0 @@
-var now     = new Date(),
-    options = {
-        year: 'numeric'
-    },
-    date    = now.toLocaleString('en-us', options),
-    year    = document.querySelector(".year");
-
-year.innerHTML = date;
