0a86d570432c154c2451c58dce26e57f8f873b05
iLyas Farawe <faraweilyas@gmail.com>
2020-07-06 17:25:16 +0100

    Deletes custom js file

--- public/assets/backend/javascript/app/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($)
-{
-    $('input[type="file"]').on('change', function(event)
-    {
-        var fileName = event.target.files[0].name;
-
-        $('.img-info').html(fileName);
-    });
-});
