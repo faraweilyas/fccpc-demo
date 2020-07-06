2d8c908c1bf187ff9eef9a75d506579dee886239
iLyas Farawe <faraweilyas@gmail.com>
2020-07-06 11:43:35 +0100

    Move functions out of event handler

--- public/assets/backend/javascript/app.js
+++ public/assets/backend/javascript/app.js
@@ -19,106 +19,6 @@ jQuery(document).ready(function($)
         $("#ffmGuideline").addClass('hide');
         $("#ffmGuideline").removeClass('show');
     });
-    function validateEpeditedOption()
-    {
-        let localGuideline          = $(".localGuideline"),
-            ffmGuideline            = $(".ffmGuideline"),
-            typeOfTransaction       = document.querySelector('#typeOfTransaction'),
-            typeOfTransactionValue  = typeOfTransaction.options[typeOfTransaction.selectedIndex].value;
-        if (typeOfTransactionValue == "local")
-        {
-            localGuideline.fadeIn('fast');
-            ffmGuideline.fadeOut('fast');
-        }
-        if (typeOfTransactionValue == "ffm")
-        {
-            localGuideline.fadeOut('fast');
-            ffmGuideline.fadeIn('fast');
-        }
-
-        if (typeOfTransactionValue == "ffx")
-        {
-            localGuideline.fadeOut('fast');
-            ffmGuideline.fadeIn('fast');
-        }
-    }
-
-    function generateFee(combinedTurnover)
-    {
-        let amount                  = Number(allowNumbers(combinedTurnover.val())),
-            fillingFee              = $(".fillingFee"),
-            processingFee           = $(".processingFee"),
-            expeditedFee            = $(".expeditedFee"),
-            totalAmount             = $(".totalAmount"),
-            expedited               = $("#expedited"),
-            result                  = 50000,
-            typeOfTransaction       = document.querySelector('#typeOfTransaction'),
-            typeOfTransactionValue  = typeOfTransaction.options[typeOfTransaction.selectedIndex].value,
-            formatter               = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2});
-
-        if (amount <= 0)
-        {
-            fillingFee.html("&#8358;0.00");
-            processingFee.html("&#8358;0.00");
-            expeditedFee.html("-");
-            totalAmount.html("&#8358;0.00");
-            return;
-        }
-        if (typeOfTransactionValue == "local")
-        {
-            if (amount >= 500000000 && amount <= 1000000000)
-            {
-                result += (0.3 / 100) * 500000000;
-                secondAmount = amount - 500000000;
-                result += (0.225 / 100) * secondAmount;
-            }
-            if (amount > 1000000000)
-            {
-                result += (0.3 / 100) * 500000000;
-                result += (0.225 / 100) * 500000000;
-                thirdAmount = amount - 1000000000;
-                result += (0.15 / 100) * thirdAmount;
-            }
-            fillingFee.html("&#8358;50,000.00");
-            processingFee.html("&#8358;"+formatter.format(result - 50000));
-            expeditedFee.html("-");
-            totalAmount.html("&#8358;"+formatter.format(result));
-        }
-        if (typeOfTransactionValue == "ffm")
-        {
-            if (amount >= 500000000 && amount < 1000000000)
-            {
-                result += 2000000;
-            }
-            if (amount >= 1000000000)
-            {
-                otherAmount = (0.1 / 100) * amount;
-                result += (otherAmount > 3000000) ? otherAmount : 3000000;
-            }
-            fillingFee.html("&#8358;50,000.00");
-            processingFee.html("&#8358;"+formatter.format(result - 50000));
-            expeditedFee.html("-");
-            totalAmount.html("&#8358;"+formatter.format(result));
-        }
-
-        if (typeOfTransactionValue == "ffx")
-        {
-            if (amount >= 500000000 && amount < 1000000000)
-            {
-                result += 2000000;
-            }
-            if (amount >= 1000000000)
-            {
-                otherAmount = (0.1 / 100) * amount;
-                result += (otherAmount > 3000000) ? otherAmount : 3000000;
-            }
-            fillingFee.html("&#8358;50,000.00");
-            processingFee.html("&#8358;"+formatter.format(result - 50000));
-                expeditedFee.html("&#8358;"+formatter.format(5000000));
-                result += 5000000;
-            totalAmount.html("&#8358;"+formatter.format(result));
-        }
-    }
 
     // on first run Hide/show expedited option based on transaction type and generate fee
     validateEpeditedOption();
@@ -145,3 +45,104 @@ jQuery(document).ready(function($)
         generateFee($("#combinedTurnover"));
     });
 });
+
+function validateEpeditedOption()
+{
+    let localGuideline          = $(".localGuideline"),
+        ffmGuideline            = $(".ffmGuideline"),
+        typeOfTransaction       = document.querySelector('#typeOfTransaction'),
+        typeOfTransactionValue  = typeOfTransaction.options[typeOfTransaction.selectedIndex].value;
+    if (typeOfTransactionValue == "local")
+    {
+        localGuideline.fadeIn('fast');
+        ffmGuideline.fadeOut('fast');
+    }
+    if (typeOfTransactionValue == "ffm")
+    {
+        localGuideline.fadeOut('fast');
+        ffmGuideline.fadeIn('fast');
+    }
+
+    if (typeOfTransactionValue == "ffx")
+    {
+        localGuideline.fadeOut('fast');
+        ffmGuideline.fadeIn('fast');
+    }
+}
+
+function generateFee(combinedTurnover)
+{
+    let amount                  = Number(allowNumbers(combinedTurnover.val())),
+        fillingFee              = $(".fillingFee"),
+        processingFee           = $(".processingFee"),
+        expeditedFee            = $(".expeditedFee"),
+        totalAmount             = $(".totalAmount"),
+        expedited               = $("#expedited"),
+        result                  = 50000,
+        typeOfTransaction       = document.querySelector('#typeOfTransaction'),
+        typeOfTransactionValue  = typeOfTransaction.options[typeOfTransaction.selectedIndex].value,
+        formatter               = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2});
+
+    if (amount <= 0)
+    {
+        fillingFee.html("&#8358;0.00");
+        processingFee.html("&#8358;0.00");
+        expeditedFee.html("-");
+        totalAmount.html("&#8358;0.00");
+        return;
+    }
+    if (typeOfTransactionValue == "local")
+    {
+        if (amount >= 500000000 && amount <= 1000000000)
+        {
+            result += (0.3 / 100) * 500000000;
+            secondAmount = amount - 500000000;
+            result += (0.225 / 100) * secondAmount;
+        }
+        if (amount > 1000000000)
+        {
+            result += (0.3 / 100) * 500000000;
+            result += (0.225 / 100) * 500000000;
+            thirdAmount = amount - 1000000000;
+            result += (0.15 / 100) * thirdAmount;
+        }
+        fillingFee.html("&#8358;50,000.00");
+        processingFee.html("&#8358;"+formatter.format(result - 50000));
+        expeditedFee.html("-");
+        totalAmount.html("&#8358;"+formatter.format(result));
+    }
+    if (typeOfTransactionValue == "ffm")
+    {
+        if (amount >= 500000000 && amount < 1000000000)
+        {
+            result += 2000000;
+        }
+        if (amount >= 1000000000)
+        {
+            otherAmount = (0.1 / 100) * amount;
+            result += (otherAmount > 3000000) ? otherAmount : 3000000;
+        }
+        fillingFee.html("&#8358;50,000.00");
+        processingFee.html("&#8358;"+formatter.format(result - 50000));
+        expeditedFee.html("-");
+        totalAmount.html("&#8358;"+formatter.format(result));
+    }
+
+    if (typeOfTransactionValue == "ffx")
+    {
+        if (amount >= 500000000 && amount < 1000000000)
+        {
+            result += 2000000;
+        }
+        if (amount >= 1000000000)
+        {
+            otherAmount = (0.1 / 100) * amount;
+            result += (otherAmount > 3000000) ? otherAmount : 3000000;
+        }
+        fillingFee.html("&#8358;50,000.00");
+        processingFee.html("&#8358;"+formatter.format(result - 50000));
+            expeditedFee.html("&#8358;"+formatter.format(5000000));
+            result += 5000000;
+        totalAmount.html("&#8358;"+formatter.format(result));
+    }
+}
