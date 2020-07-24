d757d6dfd0fb63f89471d28de67ca9951fe25fe1
iLyas Farawe <faraweilyas@gmail.com>
2020-07-24 14:43:08 +0100

    Removes unused columns from document factory

--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -10,8 +10,6 @@ $factory->define(Document::class, function(Faker $faker)
 {
     return [
         'case_id'           => factory(Cases::class),
-        'group'             => $faker->randomElement(AppHelper::keys('file_groups')),
-        'document_name'     => $faker->words(3, true),
         'file'              => $faker->imageUrl($width = 640, $height = 480),
         'additional_info'   => $faker->paragraphs(1, true),
     ];
