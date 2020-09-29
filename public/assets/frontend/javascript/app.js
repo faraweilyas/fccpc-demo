837dbaca402c678b9ad7f3ade228f835fbc22a1a
Kamsi Kodi <kamsikodi@gmail.com>
2020-09-29 12:11:50 +0100

    Added Assets FOr New UI

---
+++ public/assets/frontend/javascript/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $('#open_fee_calculator').click(function () {
+    $('#fee__calculator').toggleClass('fee__calculator--active ')
+    console.log('clicked')
+  })
+
+  $('#fee__calculator-btn').click(function () {
+    $('#fee__calculator').toggleClass('fee__calculator--active ')
+    console.log('clicked')
+  })
+
+  $('[id^=control]').each(function (index, el) {
+    $(this).click(function () {
+      $('#control-1').removeClass('card__box-stack-active')
+      $('#control-2').removeClass('card__box-stack-active')
+      $('#control-3').removeClass('card__box-stack-active')
+      $('#control-4').removeClass('card__box-stack-active')
+
+      if ($(this).attr('id') === 'control-1') {
+        $('#contact').hide()
+        $('#documents').hide()
+        $(this).addClass('card__box-stack-active')
+        $('#applications').toggle('fast')
+      }
+      if ($(this).attr('id') === 'control-2') {
+        $('#applications').hide()
+        $('#documents').hide()
+        $(this).addClass('card__box-stack-active')
+        $('#contact').toggle('fast')
+      }
+      if ($(this).attr('id') === 'control-3') {
+        $('#applications').hide()
+        $('#documents').hide()
+        $(this).addClass('card__box-stack-active')
+        $('#contact').toggle('fast')
+      }
+      if ($(this).attr('id') === 'control-4') {
+        $('#applications').hide()
+        $('#documents').hide()
+        $(this).addClass('card__box-stack-active')
+        $('#contact').toggle('fast')
+      }
+    })
+  })
+});
