3caf4a3d615f002954b21b019ebea9f8a9be785e
Kamsi Kodi <kamsikodi@gmail.com>
2020-10-05 22:25:23 +0100

    UI Modification

--- resources/views/layouts/backend/admin.blade.php
+++ resources/views/layouts/backend/admin.blade.php
@@ -39,7 +39,7 @@
 
       <ul class="menu-nav">
     
-        <li class="menu-item " aria-haspopup="true">
+        <li class="menu-item bb_home" aria-haspopup="true">
           <a href="/" class="menu-link">
             <span class="svg-icon menu-icon">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
