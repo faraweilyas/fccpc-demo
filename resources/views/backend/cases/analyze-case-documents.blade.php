b8f9af9d3a453afcc5d84347a70448c3f0d40dfb
AroicX <Arowosegbe67@gmail.com>
2020-10-09 09:58:36 +0100

    ....

--- resources/views/backend/cases/analyze-case-documents.blade.php
+++ resources/views/backend/cases/analyze-case-documents.blade.php
@@ -12,16 +12,15 @@
                     </li>
                     @if(in_array(\Auth::user()->account_type, ['SP']))
                     <li class="breadcrumb-item">
-                        <a href="{{ route('cases.unassigned') }}"
-                            class="text-muted">New Cases</a>
+                        <a href="{{ route('cases.unassigned') }}" class="text-muted">New Cases</a>
                     </li>
                     @endif
                     <li class="breadcrumb-item">
-                        <a href="{{ route('cases.assigned') }}"
-                            class="text-muted">Assigned Cases</a>
+                        <a href="{{ route('cases.assigned') }}" class="text-muted">Assigned Cases</a>
                     </li>
                     <li class="breadcrumb-item">
-                        <a href="{{ route('cases.analyze', ['case' => $case->id]) }}" class="text-muted">Analyze Case</a>
+                        <a href="{{ route('cases.analyze', ['case' => $case->id]) }}" class="text-muted">Analyze
+                            Case</a>
                     </li>
                     <li class="breadcrumb-item">
                         <a href="" class="text-muted">Checklist Documents</a>
@@ -32,34 +31,38 @@
     </div>
 </div>
 
-<div class="conatiner-fl px-5 py-5 relative">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card-custom">
-                <h5 class="text-bold">Submitted Checklist Document</h5>
-                <a href="{{ route('cases.checklist-approval',[$case->id]) }}"
-                    class="btn btn-success-transparent-download">
-                    Start Document Approval
-                </a>
+<div class="conatiner-fl px-5 py-5 ">
 
-                <div class="col-md-3 download-content py-5 my-5">
-                    @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $checklistGroup)
-                    @php
-                        $document = $checklistGroupDocuments[$checklistGroup->id] ?? '';
-                    @endphp
-                    @if($document !== '')
-                        <div class="download-card">
-                            <img src="{{ pc_asset(BE_IMAGE.'png/pdf.png') }}" alt="pdf" />
+    <div class="card-custom relative">
+        <h5 class="text-bold">Submitted Checklist Document</h5>
+        <a href="{{ route('cases.checklist-approval',[$case->id]) }}" class="btn btn-success-transparent-download">
+            Start Document Approval
+        </a>
 
-                            <p>{{ $checklistGroup->name }}</p>
-                            <button class="btn btn-success-sm" onclick="window.location.href = '{{ route('applicant.document.download', ['document' => $document->id]) }}';" >Download</button>
-                        </div>
-                        @endif
-                    @endforeach
-                </div>
+
+
+        <div class="row">
+            @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $checklistGroup)
+            @php
+            $document = $checklistGroupDocuments[$checklistGroup->id] ?? '';
+            @endphp
+            @if($document !== '')
+            <div class="col-md-4 ">
+            <div class="download-card">
+                <img src="{{ pc_asset(BE_IMAGE.'png/pdf.png') }}" alt="pdf" />
+
+                <p>{{ $checklistGroup->name }}</p>
+                <button class="btn btn-success-sm"
+                    onclick="window.location.href = '{{ route('applicant.document.download', ['document' => $document->id]) }}';">Download</button>
             </div>
-        </div>
+            </div>
+            @endif
+            @endforeach
+         
+
+
     </div>
+
 
 
 </div>
