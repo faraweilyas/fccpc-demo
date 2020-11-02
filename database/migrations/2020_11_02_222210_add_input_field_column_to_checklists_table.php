66e5b290997d75dc5a8a308f7a2e5c7bbefd2091
Kamsi Kodi <kamsikodi@gmail.com>
2020-11-02 23:29:05 +0100

    Configured Migrations For Fees

---
+++ database/migrations/2020_11_02_222210_add_input_field_column_to_checklists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInputFieldColumnToChecklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('checklists', function (Blueprint $table) {
+            $table->boolean('input_field')->default(false)->after('label');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('checklists', function (Blueprint $table) {
+            $table->dropColumn('input_field');
+        });
+    }
+}
