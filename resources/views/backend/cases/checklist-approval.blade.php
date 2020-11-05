1b018af56409f21a49b5230c66af793a817f5a9f
iLyas Farawe <faraweilyas@gmail.com>
2020-11-05 03:10:24 +0100

    Fixes checklist approval bug

--- resources/views/backend/cases/checklist-approval.blade.php
+++ resources/views/backend/cases/checklist-approval.blade.php
@@ -38,114 +38,111 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card-custom">
-                    @php $x = 1 @endphp
                     @php
-                    $deficient_count = $checklistStatusCount->deficient ?? 0;
+                        $x                  = 1;
+                        $deficient_count    = $checklistStatusCount->deficient ?? 0;
                     @endphp
+
                     @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $checklistGroup)
-                    @php
-                    $document = $checklistGroupDocuments[$checklistGroup->id] ?? '';
-                    @endphp
-                    @if($document !== '')
-                    <div class="row my-3 py-5 hide" id="step-{{ $x }}">
 
-                        <h5 class="text-bold w-50">
+                        @php $document = $checklistGroupDocuments[$checklistGroup->id] ?? ''; @endphp
+                        @if ($document !== '')
+                            <div class="row my-3 py-5 hide" id="step-{{ $x }}">
+                                <h5 class="text-bold w-50">
 
-                            {{ $checklistGroup->name }}
-                            <div class="pull-button-right">
-                                <button id="deficient-basket" class="btn btn-light-primary font-weight-bold mx-lg-5 py-3" data-toggle="modal"
-                                    data-target="#Issue" data-case-id="{{ $case->id }}">
-                                    <span class="svg-icon svg-icon-xl">
+                                    {{ $checklistGroup->name }}
+                                    <div class="pull-button-right">
+                                        <button id="deficient-basket" class="btn btn-light-primary font-weight-bold mx-lg-5 py-3" data-toggle="modal"
+                                            data-target="#Issue" data-case-id="{{ $case->id }}">
+                                            <span class="svg-icon svg-icon-xl">
 
-                                        <!--begin::Svg Icon | path:assets/media/svg/icons/Shopping/Cart3.svg-->
-                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                <rect x="0" y="0" width="20" height="20"></rect>
-                                                <path d="M12,4.56204994 L7.76822128,9.6401844 C7.4146572,10.0644613 6.7840925,10.1217854 6.3598156,9.76822128 C5.9355387,9.4146572 5.87821464,8.7840925 6.23177872,8.3598156 L11.2317787,2.3598156 C11.6315738,1.88006147 12.3684262,1.88006147 12.7682213,2.3598156 L17.7682213,8.3598156 C18.1217854,8.7840925 18.0644613,9.4146572 17.6401844,9.76822128 C17.2159075,10.1217854 16.5853428,10.0644613 16.2317787,9.6401844 L12,4.56204994 Z" fill="#fff" fill-rule="nonzero" opacity="0.3"></path>
-                                                <path d="M3.5,9 L20.5,9 C21.0522847,9 21.5,9.44771525 21.5,10 C21.5,10.132026 21.4738562,10.2627452 21.4230769,10.3846154 L17.7692308,19.1538462 C17.3034221,20.271787 16.2111026,21 15,21 L9,21 C7.78889745,21 6.6965779,20.271787 6.23076923,19.1538462 L2.57692308,10.3846154 C2.36450587,9.87481408 2.60558331,9.28934029 3.11538462,9.07692308 C3.23725479,9.02614384 3.36797398,9 3.5,9 Z M12,17 C13.1045695,17 14,16.1045695 14,15 C14,13.8954305 13.1045695,13 12,13 C10.8954305,13 10,13.8954305 10,15 C10,16.1045695 10.8954305,17 12,17 Z" fill="#fff"></path>
-                                            </g>
-                                        </svg>
+                                                <!--begin::Svg Icon | path:assets/media/svg/icons/Shopping/Cart3.svg-->
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                        <rect x="0" y="0" width="20" height="20"></rect>
+                                                        <path d="M12,4.56204994 L7.76822128,9.6401844 C7.4146572,10.0644613 6.7840925,10.1217854 6.3598156,9.76822128 C5.9355387,9.4146572 5.87821464,8.7840925 6.23177872,8.3598156 L11.2317787,2.3598156 C11.6315738,1.88006147 12.3684262,1.88006147 12.7682213,2.3598156 L17.7682213,8.3598156 C18.1217854,8.7840925 18.0644613,9.4146572 17.6401844,9.76822128 C17.2159075,10.1217854 16.5853428,10.0644613 16.2317787,9.6401844 L12,4.56204994 Z" fill="#fff" fill-rule="nonzero" opacity="0.3"></path>
+                                                        <path d="M3.5,9 L20.5,9 C21.0522847,9 21.5,9.44771525 21.5,10 C21.5,10.132026 21.4738562,10.2627452 21.4230769,10.3846154 L17.7692308,19.1538462 C17.3034221,20.271787 16.2111026,21 15,21 L9,21 C7.78889745,21 6.6965779,20.271787 6.23076923,19.1538462 L2.57692308,10.3846154 C2.36450587,9.87481408 2.60558331,9.28934029 3.11538462,9.07692308 C3.23725479,9.02614384 3.36797398,9 3.5,9 Z M12,17 C13.1045695,17 14,16.1045695 14,15 C14,13.8954305 13.1045695,13 12,13 C10.8954305,13 10,13.8954305 10,15 C10,16.1045695 10.8954305,17 12,17 Z" fill="#fff"></path>
+                                                    </g>
+                                                </svg>
 
-                                        <!--end::Svg Icon-->
-                                        <span class="checklist-deficient-count">{{ $checklistStatusCount->deficient ?? 0 }}</span>
-                                        {{-- Deficiencies --}}
-                                        {{-- <span class="checklist-deficient-text">Deficiencies</span> --}}
-                                    </span>
-                                </button>
+                                                <!--end::Svg Icon-->
+                                                <span class="checklist-deficient-count">{{ $checklistStatusCount->deficient ?? 0 }}</span>
+                                                {{-- Deficiencies --}}
+                                                {{-- <span class="checklist-deficient-text">Deficiencies</span> --}}
+                                            </span>
+                                        </button>
 
-                                <button class="btn btn-success no-border px-10 py-4"
-                                    onclick="window.location.href = '{{ route('applicant.document.download', ['document' => $document->id]) }}';">
-                                    Download Checklist Document
-                                </button>
-                            </div>
-                        </h5>
-
-                        <div class="row py-5 margin-top">
-                            <div class="col-md-12">
-                                <p><b>Additional Information:</b></p>
-                                <p>
-                                    @empty($document->additional_info) ... @else {{ $document->additional_info }} @endif
-                                </p>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            @foreach($checklistGroup->checklists as $checklist)
-                            @php
-                            $checklist_document = $document->checklists->where('id', $checklist->id)->first()->checklist_document ?? NULL;
-                            $checklist_document_status = $checklist_document->status ?? NULL;
-                            $checked = (in_array($checklist->id, $checklistIds) && !is_null($checklist_document->selected_at)) ? "consent-card-active" : '';
-                            @endphp
-                            <div class="col-lg-6">
-                                <div class="consent-card {{ $checked }}">
-                                    <div class="d-flex">
-                                        <div class="form-check" style="padding: 0px">
-                                            <div class="radio-inline">
-                                                @empty($checked)
-                                                <span class="switch switch-sm">
-                                                    <label>
-                                                        <input type="checkbox" class="save_approval" name="select" @if($checklist_document_status=='deficient' ) checked="checked" @endif value="deficient"
-                                                        @if($checklist_document_status=='deficient' ) checked="checked"
-                                                        @endif data-document-id="{{ $document->id }}"
-                                                        data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="true">
-                                                        <span></span>
-                                                    </label>
-                                                    Deficient
-                                                </span>
-                                                @else
-                                                <label class="radio">
-                                                    <input class="form-check-input save_approval" type="radio"
-                                                        name="exampleRadios{{ $checklist->id }}" value="approved"
-                                                        @if($checklist_document_status=='approved' ) checked="checked"
-                                                        @endif data-document-id="{{ $document->id }}"
-                                                        data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="false">
-                                                    <span></span>
-                                                    Approve
-                                                </label>
-                                                <label class="radio">
-                                                    <input class="form-check-input save_approval" type="radio"
-                                                        name="exampleRadios{{ $checklist->id }}" value="deficient"
-                                                        @if($checklist_document_status=='deficient' ) checked="checked"
-                                                        @endif data-document-id="{{ $document->id }}"
-                                                        data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="false">
-                                                    <span></span>
-                                                    Deficient
-                                                </label>
-                                                @endif
+                                        <button class="btn btn-success no-border px-10 py-4"
+                                            onclick="window.location.href = '{{ route('applicant.document.download', ['document' => $document->id]) }}';">
+                                            Download Checklist Document
+                                        </button>
+                                    </div>
+                                </h5>
+                                <div class="row py-5 margin-top">
+                                    <div class="col-md-12">
+                                        <p><b>Additional Information:</b></p>
+                                        <p>
+                                            @empty($document->additional_info) ... @else {{ $document->additional_info }} @endif
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    @foreach($checklistGroup->checklists as $checklist)
+                                    @php
+                                        $checklist_document = $document->checklists->where('id', $checklist->id)->first()->checklist_document ?? NULL;
+                                        $checklist_document_status = $checklist_document->status ?? NULL;
+                                        $checked = (in_array($checklist->id, $checklistIds) && !is_null($checklist_document->selected_at ?? NULL)) ? "consent-card-active" : '';
+                                    @endphp
+                                    <div class="col-lg-6">
+                                        <div class="consent-card {{ $checked }}">
+                                            <div class="d-flex">
+                                                <div class="form-check" style="padding: 0px">
+                                                    <div class="radio-inline">
+                                                        @empty($checked)
+                                                        <span class="switch switch-sm">
+                                                            <label>
+                                                                <input type="checkbox" class="save_approval" name="select" @if($checklist_document_status=='deficient' ) checked="checked" @endif value="deficient"
+                                                                @if($checklist_document_status=='deficient' ) checked="checked"
+                                                                @endif data-document-id="{{ $document->id }}"
+                                                                data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="true">
+                                                                <span></span>
+                                                            </label>
+                                                            Deficient
+                                                        </span>
+                                                        @else
+                                                        <label class="radio">
+                                                            <input class="form-check-input save_approval" type="radio"
+                                                                name="exampleRadios{{ $checklist->id }}" value="approved"
+                                                                @if($checklist_document_status=='approved' ) checked="checked"
+                                                                @endif data-document-id="{{ $document->id }}"
+                                                                data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="false">
+                                                            <span></span>
+                                                            Approve
+                                                        </label>
+                                                        <label class="radio">
+                                                            <input class="form-check-input save_approval" type="radio"
+                                                                name="exampleRadios{{ $checklist->id }}" value="deficient"
+                                                                @if($checklist_document_status=='deficient' ) checked="checked"
+                                                                @endif data-document-id="{{ $document->id }}"
+                                                                data-checklist-id="{{ $checklist->id }}" data-case-id="{{ $case->id }}" data-switch-box="false">
+                                                            <span></span>
+                                                            Deficient
+                                                        </label>
+                                                        @endif
+                                                    </div>
+                                                </div>
                                             </div>
+                                            <p>
+                                                {{ ucfirst($checklist->name) }}
+                                            </p>
                                         </div>
                                     </div>
-                                    <p>
-                                        {{ ucfirst($checklist->name) }}
-                                    </p>
+                                    @endforeach
                                 </div>
                             </div>
-                            @endforeach
-                        </div>
-                    </div>
-                    @php $x++ @endphp
-                    @endif
-                    <input class="checklist_group_count" type="hidden" value="{{ count($checklistGroupDocuments) }}">
+                            @php $x++ @endphp
+                        @endif
+                        <input class="checklist_group_count" type="hidden" value="{{ count($checklistGroupDocuments) }}" />
                     @endforeach
                     <div class="btn-group">
                         <button class="btn btn-success-pale-ts no-border mx-1 px-10 py-4" id="prev">
