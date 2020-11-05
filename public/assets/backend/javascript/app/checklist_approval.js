7b5d4cbcf76d818d33869001b8cc42c31aeda3cb
iLyas Farawe <faraweilyas@gmail.com>
2020-11-05 03:07:28 +0100

    Fixes lagging in deficiency counter

--- public/assets/backend/javascript/app/checklist_approval.js
+++ public/assets/backend/javascript/app/checklist_approval.js
@@ -1,179 +1,184 @@
- var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-    var counter    = 1;
-    var arr_lenght = $(".checklist_group_count").val();
+ var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content'),
+    counter     = 1,
+    arr_lenght  = $(".checklist_group_count").val();
 
-    console.log(arr_lenght);
+$(document).ready(function ()
+{
+    $('#step-1').show();
+    $('#prev').hide();
+    $('#prev').click(function()
+    {
+        if (counter > 1) {
+            $('#next').show();
+            $('#approve').hide();
+            $('#deficiency').hide();
+            counter--;
+            $('[id^=step]').hide();
+            $(`#step-${counter}`).show();
+            $(window).scrollTop(0);
+        } else {
+            $('#next').show();
+            $('#approve').hide();
+            counter = 1;
+            $('[id^=step]').hide();
+            $(`#step-${counter}`).show();
+            $(window).scrollTop(0);
+            return false;
+        }
 
-    $(document).ready(function () {
-        $('#step-1').show();
-        $('#prev').hide();
-        $('#prev').click(function () {
-            if (counter > 1) {
-                $('#next').show();
-                $('#approve').hide();
-                $('#deficiency').hide();
-                counter--;
-                $('[id^=step]').hide();
-                $(`#step-${counter}`).show();
-                $(window).scrollTop(0);
-            } else {
-                $('#next').show();
-                $('#approve').hide();
-                counter = 1;
-                $('[id^=step]').hide();
-                $(`#step-${counter}`).show();
-                $(window).scrollTop(0);
-                return false;
-            }
+        if (counter === 1) $('#prev').hide();
+    });
 
+    $('#next').click(function(event)
+    {
+        event.preventDefault();
+        // toastr.success("cool");
+        if (counter < arr_lenght) {
+            $('#prev').show();
 
-            if (counter === 1) {
-                $('#prev').hide();
+            counter++;
+            $('[id^=step]').hide();
+            $(`#step-${counter}`).show();
+            $(window).scrollTop(0);
+        }
 
-            }
+        if (parseInt(counter) === parseInt(arr_lenght))
+        {
+            $('#next').hide();
+            $('#approve').show();
+            $('#deficiency').show();
+        }
+    });
 
-        })
-        $('#next').click(function (event) {
-            event.preventDefault();
-            // toastr.success("cool");
-            if (counter < arr_lenght) {
-                $('#prev').show();
+    $(".save_approval").on('change', function()
+    {
+        var formData            = new FormData(),
+            case_id             = $(this).attr('data-case-id'),
+            doc_id              = $(this).attr('data-document-id'),
+            checklist_id        = $(this).attr('data-checklist-id'),
+            switch_box          = $(this).attr('data-switch-box');
+            remove_checklist    = '',
+            status              = '';
 
-                counter++;
-                $('[id^=step]').hide();
-                $(`#step-${counter}`).show();
-                $(window).scrollTop(0);
+        if ($(this).is(":checked") && switch_box === "true") {
+            remove_checklist = 'no';
+            status           = $(this).val();
+        } else if(!$(this).is(":checked") && switch_box === "true") {
+            remove_checklist = 'yes';
+            status           = '';
+        } else {
+            remove_checklist = 'no';
+            status           = $(this).val();
+        }
 
-            }
-
-            if (parseInt(counter) === parseInt(arr_lenght)) {
-                $('#next').hide();
-                $('#approve').show();
-                $('#deficiency').show();
-            }
-
-
-        });
-
-        $(".save_approval").on('change', function(){
-            var formData         = new FormData(),
-                case_id          = $(this).attr('data-case-id'),
-                doc_id           = $(this).attr('data-document-id'),
-                checklist_id     = $(this).attr('data-checklist-id'),
-                switch_box       = $(this).attr('data-switch-box');
-                var remove_checklist;
-                var status;
-                if($(this).is(":checked") && switch_box === "true") {
-                    remove_checklist = 'no';
-                    status           = $(this).val();
-                } else if(!$(this).is(":checked") && switch_box === "true") {
-                    remove_checklist = 'yes';
-                    status           = '';
-                } else {
-                    remove_checklist = 'no';
-                    status           = $(this).val();
-                };
-
-            $.ajax({
-                url: '/cases/checklist-approval/'+doc_id,
-                type: 'POST',
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {checklist: checklist_id, status: status, remove_checklist: remove_checklist}, 
-                success: function(response){
-                  // console.log(response);
-                }
-            });
-
-            $.ajax({
-                url: '/cases/checklist-status-count/'+case_id,
-                type: 'GET',
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {}, 
-                success: function(response){
-                    var result = JSON.parse(response);
-                    if (result.response.deficient === undefined)
+        $.ajax({
+            url: '/cases/checklist-approval/'+doc_id,
+            type: 'POST',
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            data: {
+                checklist: checklist_id,
+                status: status,
+                remove_checklist: remove_checklist
+            },
+            success: function(response)
+            {
+                $.ajax({
+                    url: '/cases/checklist-status-count/'+case_id,
+                    type: 'GET',
+                    headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                    data: {},
+                    success: function(response)
                     {
-                        $(".checklist-deficient-count").html('0');
-                    } else {
-                        $(".checklist-deficient-count").html(result.response.deficient);
+                        var result = JSON.parse(response);
+                        if (result.response.deficient === undefined)
+                        {
+                            $(".checklist-deficient-count").html('0');
+                        } else {
+                            $(".checklist-deficient-count").html(result.response.deficient);
+                        }
                     }
-                }
-            });
-        });
-
-        $("#deficient-basket").on('click', function (event) {
-            var case_id = $(this).attr('data-case-id');
-            $("#deficient_cases_list div").empty();
-            $.ajax({
-                url: '/cases/checklist-by-status/'+case_id,
-                type: 'GET',
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {}, 
-                success: function(response){
-                    var result          = JSON.parse(response);
-                    var deficient_cases = result.response.deficent_cases;
-                    $.each(deficient_cases, function(index, value){
-                        $("#deficient_cases_list").append('<div>'
-                                                            +'<p class="alert-custom ">'
-                                                            +value.name
-                                                            +'</p>'
-                                                            +'</div>'
-                                                        );
-                    });
-                }
-            });
-        });
-
-        $('#viewDeficiencyModal').on('shown.bs.modal', function (event) {
-            var thisModal               = $(this),
-                case_id                 = $('.case_id').html();
-                applicant_firm          = thisModal.find('#applicant_firm'),
-                applicant_name          = thisModal.find('#applicant_name'),
-                applicant_email         = thisModal.find('#applicant_email'),
-                applicant_phone_number  = thisModal.find('#applicant_phone_number'),
-                applicant_address       = thisModal.find('#applicant_address');
-                issue_deficiency_button = thisModal.find('#issue-deficiency');
-
-            // Get Case Deficiencies Asynchronously
-            $.ajax({
-                url: '/cases/checklist-by-status/'+case_id,
-                type: "GET",
-                success: function (response) {
-                    var result = JSON.parse(response);
-                    $("#deficiency_items").empty();
-                    $.each(result.response.deficent_cases, function (index, value) {
-                        $("#deficiency_items").append('<div class="d-flex align-items-center justify-content-start mb-2">' +
-                            '<span class="icon-1x mr-2"><b>' +
-                            (index + 1) + '.</b> ' + value.name +
-                            '</span>' +
-                            '</div>');
-                    });
-                },
-            });
-
-            issue_deficiency_button.attr('data-case-id', case_id);
-            applicant_firm.html($('.firm').html());
-            applicant_name.html($('.name').html());
-            applicant_email.html($('.email').html());
-            applicant_phone_number.html($('.phone_number').html());
-            applicant_address.html($('.address').html());
-            return;
-        });
-
-        $('#issue-deficiency').on('click', function (event) {
-            $("#saving-deficiency").removeClass('hide');
-            $('#issue-deficiency').addClass('hide');
-            $.ajax({
-                url: '/cases/issue-deficiency/'+$(this).attr('data-case-id'),
-                type: 'POST',
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                data: {additional_info: $("#additional_info").val()}, 
-                success: function(response){
-                    var result          = JSON.parse(response);
-                    $("#saving-deficiency").addClass('hide');
-                    $('#issue-deficiency').removeClass('hide');
-                    toastr.success("Email sent to applicant");
-                }
-            });
+                });
+            }
         });
     });
+
+    $("#deficient-basket").on('click', function(event)
+    {
+        var case_id = $(this).attr('data-case-id');
+        $("#deficient_cases_list div").empty();
+
+        $.ajax({
+            url: '/cases/checklist-by-status/'+case_id,
+            type: 'GET',
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            data: {},
+            success: function(response)
+            {
+                var result          = JSON.parse(response),
+                    deficient_cases = result.response.deficent_cases;
+
+                $.each(deficient_cases, function(index, value)
+                {
+                    $("#deficient_cases_list").append('<div><p class="alert-custom">'+value.name+'</p></div>');
+                });
+            }
+        });
+    });
+
+    $('#viewDeficiencyModal').on('shown.bs.modal', function(event)
+    {
+        var thisModal               = $(this),
+            case_id                 = $('.case_id').html();
+            applicant_firm          = thisModal.find('#applicant_firm'),
+            applicant_name          = thisModal.find('#applicant_name'),
+            applicant_email         = thisModal.find('#applicant_email'),
+            applicant_phone_number  = thisModal.find('#applicant_phone_number'),
+            applicant_address       = thisModal.find('#applicant_address');
+            issue_deficiency_button = thisModal.find('#issue-deficiency');
+
+        // Get Case Deficiencies Asynchronously
+        $.ajax({
+            url: '/cases/checklist-by-status/'+case_id,
+            type: "GET",
+            success: function (response) {
+                var result = JSON.parse(response);
+                $("#deficiency_items").empty();
+                $.each(result.response.deficent_cases, function(index, value)
+                {
+                    $("#deficiency_items").append('<div class="d-flex align-items-center justify-content-start mb-2">' +
+                        '<span class="icon-1x mr-2"><b>' +
+                        (index + 1) + '.</b> ' + value.name +
+                        '</span>' +
+                        '</div>');
+                });
+            },
+        });
+
+        issue_deficiency_button.attr('data-case-id', case_id);
+        applicant_firm.html($('.firm').html());
+        applicant_name.html($('.name').html());
+        applicant_email.html($('.email').html());
+        applicant_phone_number.html($('.phone_number').html());
+        applicant_address.html($('.address').html());
+        return;
+    });
+
+    $('#issue-deficiency').on('click', function(event)
+    {
+        $("#saving-deficiency").removeClass('hide');
+        $('#issue-deficiency').addClass('hide');
+        $.ajax({
+            url: '/cases/issue-deficiency/'+$(this).attr('data-case-id'),
+            type: 'POST',
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            data: {additional_info: $("#additional_info").val()},
+            success: function(response)
+            {
+                var result = JSON.parse(response);
+                $("#saving-deficiency").addClass('hide');
+                $('#issue-deficiency').removeClass('hide');
+                toastr.success("Email sent to applicant");
+            }
+        });
+    });
+});
