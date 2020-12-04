45a1a189d65d50ad20d98e9bb561e134887c618f
Kamsi Kodi <kamsikodi@gmail.com>
2020-12-04 09:39:30 +0100

    UI Modification

--- resources/views/backend/cases/unassigned.blade.php
+++ resources/views/backend/cases/unassigned.blade.php
@@ -19,7 +19,7 @@
     </div>
 </div>
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-    <div class="d-flex flex-column-fluid mt-sm-30 mt-md-30 md-lg-0">
+    <div class="d-flex flex-column-fluid">
         <div class="container">
             <div class="card card-custom">
                 <div class="card-header flex-wrap py-5">
