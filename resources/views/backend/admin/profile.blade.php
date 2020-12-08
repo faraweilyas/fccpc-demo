cc1646688b917efd65f7b5789cf6f96245e81394
iLyas Farawe <faraweilyas@gmail.com>
2020-12-08 09:33:15 +0100

    Adds card for case handler stats

--- resources/views/backend/admin/profile.blade.php
+++ resources/views/backend/admin/profile.blade.php
@@ -24,6 +24,99 @@
                 <div class="col-md-12 my-5">
                     <div class="card card__box__large">
                         <div class="card__box__large-content">
+                            <div class='row'>
+                                <div class='col-md-12'>
+                                    <div class="card card-stretch gutter-b" style="border: none !important;">
+                                        <div class="card-body d-flex flex-column" style="position: relative;">
+                                            <div class="mt-10 mb-5">
+                                                <div class="row row-paddingless mb-10">
+                                                    <div class="col">
+                                                        <div class="d-flex align-items-center mr-2">
+                                                            <div class="symbol symbol-45 symbol-light-info mr-4 flex-shrink-0">
+                                                                <div class="symbol-label">
+                                                                    <span class="svg-icon svg-icon-lg svg-icon-info">
+                                                                        <x-icons.basket></x-icons.basket>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="font-size-h4 text-dark-75 font-weight-bolder">24</div>
+                                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Assigned Cases</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="d-flex align-items-center mr-2">
+                                                            <div class="symbol symbol-45 symbol-light-danger mr-4 flex-shrink-0">
+                                                                <div class="symbol-label">
+                                                                    <span class="svg-icon svg-icon-lg svg-icon-danger">
+                                                                        <x-icons.lines></x-icons.lines>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="font-size-h4 text-dark-75 font-weight-bolder">706</div>
+                                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Cases On Hold</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="d-flex align-items-center mr-2">
+                                                            <div class="symbol symbol-45 symbol-light-success mr-4 flex-shrink-0">
+                                                                <div class="symbol-label">
+                                                                    <span class="svg-icon svg-icon-lg svg-icon-success">
+                                                                        <x-icons.basket></x-icons.basket>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="font-size-h4 text-dark-75 font-weight-bolder">49</div>
+                                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Ongoing Cases</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="d-flex align-items-center mr-2">
+                                                            <div class="symbol symbol-45 symbol-light-primary mr-4 flex-shrink-0">
+                                                                <div class="symbol-label">
+                                                                    <span class="svg-icon svg-icon-lg svg-icon-primary">
+                                                                        <x-icons.square></x-icons.square>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="font-size-h4 text-dark-75 font-weight-bolder">5</div>
+                                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Approved Cases</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="d-flex align-items-center mr-2">
+                                                            <div class="symbol symbol-45 symbol-light-primary mr-4 flex-shrink-0">
+                                                                <div class="symbol-label">
+                                                                    <span class="svg-icon svg-icon-lg svg-icon-primary">
+                                                                        <x-icons.square></x-icons.square>
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div class="font-size-h4 text-dark-75 font-weight-bolder">55</div>
+                                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Archived Cases</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="resize-triggers">
+                                                <div class="expand-trigger">
+                                                    <div style="width: 414px; height: 429px;"></div>
+                                                </div>
+                                                <div class="contract-trigger"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="profile-image">{{ $user->getInitials() }}</div>
@@ -71,7 +164,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="content-box ">
+                                    <div class="content-box">
                                         <span>Address</span>
                                         <p>{{ $user->address ?? '...' }}</p>
                                     </div>
