862e2b0eb042d7cb0448990ba7520367a48acc54
Kamsi Kodi <kamsikodi@gmail.com>
2021-01-01 09:29:12 +0100

    Modified Frontend Footer

--- resources/views/layouts/backend/old/user.blade.php
+++ resources/views/layouts/backend/old/user.blade.php
@@ -107,13 +107,17 @@
                 </div>
                 @yield('content')
                 <div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
-                    <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
-                        <div class="text-dark order-2 order-md-1">
-                            <span class="text-muted font-weight-bold mr-2">{{ date('Y') }} &copy;</span>
-                            <a href="{{ COMPANY_LINK }}" target="_blank" class="footer-brand-logo">
-                                <img class="max-h-20px" src="{{ BE_IMAGE.'logo/techbarn-logo.png' }}" alt="techbarn logo" />
-                            </a>
-                        </div>
+                    <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-md-8">
+                            <span style="font-size: 1rem">This is the official website of the Federal Competition and Consumer Protection Commission (FCCPC)
+                              <br>Copyright © 2020 Federal Competition and Consumer Protection Commission</span>
+                          </div>
+                          <div class="col-md-4">
+                            <br/>
+                            <span class="float-right float-right-inverse" style="font-size: 1rem">  Powered by <a href="https://techbarn.ng/" target="_blank" class="text-dark">techbarn </span>
+
+                          </div>
                     </div>
                 </div>
             </div>
