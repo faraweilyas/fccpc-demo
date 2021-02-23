72978fdaf2caaded508c33cad9fe156abe9d805e
Kamsi Kodi <kamsikodi@gmail.com>
2021-02-23 21:52:23 +0100

    Reverted Last Commit

--- routes/web/web.php
+++ routes/web/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/phpmyadmin', function ()
-{
-    return 'working';
-});
-
 Route::name('home.')
     ->namespace('Frontend')
     ->group(function()
