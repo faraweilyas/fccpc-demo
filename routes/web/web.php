1d3902ce6a1b85f092a3e4150082688560ddf089
iLyas Farawe <faraweilyas@gmail.com>
2021-08-12 16:48:41 +0100

    Adds test code for mail notifications

--- routes/web/web.php
+++ routes/web/web.php
@@ -13,6 +13,31 @@ Route::name('home.')
         })
         ->name("test");
 
+        Route::get('/notification', function()
+        {
+            $supervisor     = App\Models\User::find(5);
+            $caseHandler    = App\Models\User::find(6);
+            $case           = App\Models\Cases::find(114);
+            $enquiry        = App\Models\Enquiry::find(37);
+            // $action         = "newcase";
+            // $message        = "{$case->applicant_fullname} has submitted a new notification.";
+            // $action         = "assign";
+            // $message        = "A new case has been assigned to you.";
+            // $action         = "defresponse";
+            // $message        = "Applicant has responded to deficient documents.";
+            // $action         = "request_approved";
+            // $message        = "Your approval request has been approved.";
+            // $action         = "request_rejected";
+            // $message        = "Your approval request has been rejected.";
+            // $action         = "request";
+            // $message        = "{$caseHandler->getFullName()} has requested approval.";
+            $action         = "newenquiry";
+            $message        = "iLyas Farawe has applied for a pre-notification consultation.";
+
+            return (new App\Notifications\CaseActionNotifier($action, $message, $enquiry->id, $enquiry))->toMail($supervisor);
+            // return (new App\Notifications\NotifyHandlerForDeficientCaseSubmission($action, $message, $case))->toMail($supervisor);
+        });
+
         Route::get(
             '/',
             'HomeController@index'
